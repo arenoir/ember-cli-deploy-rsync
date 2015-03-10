a8a9ea9ab5c1e5dc3fb76b22f4603f355060e434
Aaron Renoir <aaron@fieldphone.com>
2015-03-09 17:44:34 -0700

    inital commit of asset rsync adapter.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var rsyncAdapter = require('./lib/rsync');
+
 module.exports = {
-  name: 'ember-cli-deploy-rsync'
-};
+  name: 'ember-cli-deploy-rsync',
+  type: 'ember-deploy-addon',
+  adapters: {
+    assets: {
+      'rsync': rsyncAdapter
+    }
+  }
+}
